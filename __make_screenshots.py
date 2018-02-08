165f4dc9ef3f139502c5905e3ac6c4a6b4bfec34
paintdog <paintdog@users.noreply.github.com>
2018-02-08 21:29:11 +0100

    Add files via upload

---
+++ __make_screenshots.py
@@ -0,0 +1,10 @@
+import pyscreeze
+import time
+
+while True:
+    time.sleep(0.5)
+    
+    lt = time.localtime()
+    new_file = time.strftime("%Y-%m-%d %H-%M-%S", lt) + ".png"
+
+    image = pyscreeze.screenshot(new_file)
