ca4cf481c4999957f8b375c7717b4593b5785450
paintdog <paintdog@users.noreply.github.com>
2018-02-08 21:25:09 +0100

    Update __duplicates_finder.py

--- __duplicates_finder.py
+++ __duplicates_finder.py
@@ -2,24 +2,16 @@
 import os
 import hashlib
 
-
 files = [file for file in os.listdir() if file.endswith(".png")]
-
 
 old_file = None
 old_file_checksum = None
 
 for file in files:
-
     checksum = hashlib.md5(open(file,'rb').read()).hexdigest()
-
     if checksum == old_file_checksum:
-
         print(old_file, file)
-
         os.rename(old_file, "XXX {}".format(old_file))
-
 
     old_file = file
     old_file_checksum = checksum
-
