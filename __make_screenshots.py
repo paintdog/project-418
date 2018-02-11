6050fab1d5645998b41181973564738e5f324f85
paintdog <paintdog@users.noreply.github.com>
2018-02-11 13:38:21 +0100

    Update __make_screenshots.py

--- __make_screenshots.py
+++ __make_screenshots.py
@@ -2,7 +2,7 @@ import pyscreeze
 import time
 
 while True:
-    time.sleep(0.5)
+    time.sleep(0.2)
     
     lt = time.localtime()
     new_file = time.strftime("%Y-%m-%d %H-%M-%S", lt) + ".png"
