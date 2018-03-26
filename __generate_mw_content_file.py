b1e5f1d82a33cf0f07f5a434b02162fcb85ac922
paintdog <paintdog@users.noreply.github.com>
2018-03-26 18:05:32 +0200

    Update __generate_mw_content_file.py

--- __generate_mw_content_file.py
+++ __generate_mw_content_file.py
@@ -5,5 +5,6 @@ print("{| class='wikitable' width='100%'")
 print("! style='width=80 %;' | Chapter\n! style='width=20 %;' | Page")
 for line in content:
     chapter, page = line.split(";")
+    chapter = chapter.replace("<b>","'''").replace("</b>","'''")
     print("|-\n| {}\n| style='text-align: right;' | {}".format(chapter, page))
 print("|}")
