ee60114e7c255f6fe56d58ac49132180adc0d7b6
paintdog <paintdog@users.noreply.github.com>
2018-03-27 11:20:53 +0200

    Update __generate_mw_content_file.py

--- __generate_mw_content_file.py
+++ __generate_mw_content_file.py
@@ -4,7 +4,8 @@ content = content.split("\n")
 print("{| class='wikitable' width='100%'")
 print("! style='width=80 %;' | Chapter\n! style='width=20 %;' | Page")
 for line in content:
-    chapter, page = line.split(";")
-    chapter = chapter.replace("<b>","'''").replace("</b>","'''")
-    print("|-\n| {}\n| style='text-align: right;' | {}".format(chapter, page))
+    if not line == "\n":
+        chapter, page = line.split(";")
+        chapter = chapter.replace("<b>","'''").replace("</b>","'''")
+        print("|-\n| {}\n| style='text-align: right;' | {}".format(chapter, page))
 print("|}")
